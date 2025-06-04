83e5eae3ee83ed918ac8b1408a2d3d3ad0165013
Matvey <example@gmail.com>
2025-06-04 17:03:04 +0300

    create components for my projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState, type MouseEvent } from 'react'
-import './App.css'
-import Button from './components/Button/Button'
+import { type MouseEvent } from "react";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 function App() {
-  const [counter, setCounter] = useState<number>(0)
-
   const addCounter = (e: MouseEvent) => {
-    console.log(e)
-  }
+    console.log(e);
+  };
 
   return (
     <>
       <Button onClick={addCounter}>Кнопка</Button>
+      <Button appearence="big" onClick={addCounter}>
+        Кнопка
+      </Button>
+      <Input />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,23 @@
-import "./Button.module.css"
+import styles from "./Button.module.css";
 import type { ButtonProps } from "./Button.props";
-import cn from 'classnames'
+import cn from "classnames";
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
-    return (
-        <button className={cn('button accent', className)} {...props}>{children}</button>
-    );
+const Button = ({
+  children,
+  className,
+  appearence = "small",
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={cn(styles["button"], styles["accent"], className, {
+        [styles["small"]]: appearence == "small",
+        [styles["big"]]: appearence == "big",
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 export default Button;
