5f021c77d07cedfe385b758bf893178248f253fc
Matvey <example@gmail.com>
2025-06-07 11:50:23 +0300

    demo product card

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,14 +1,27 @@
 import Headling from "../../components/Headling/Headling";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
-import styles from './Menu.module.css'
+import styles from "./Menu.module.css";
 
 const Menu = () => {
-    return (
-        <div className={styles['head']}>
-			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав' />
-		</div>
-    );
+  return (
+    <>
+      <div className={styles["head"]}>
+        <Headling>Меню</Headling>
+        <Search placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          image="/demo.png"
+          price={300}
+          rating={4.5}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Menu;
